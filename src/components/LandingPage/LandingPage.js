892d6f3d79b2a3f18efb30a2f4026ac1732debfa
Logan Wood <lwood3499@gmail.com>
2019-02-14 22:22:54 -0600

    hover description on LandingPage

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { Component } from 'react';
 
-import cls from "./LandingPage.module.css";
+import cls from './LandingPage.module.css';
 
-const landingPage = () => {
-	return (
-		<div className={cls.LoadingPageContainer}>
-			<div className={cls.myInfoContainer}>
-				<h1 className={cls.myName}>Logan Wood</h1>
-				<p className={cls.myDescription}>Educator - Developer - Problem Solver</p>
-				<div className={cls.nextSectionArrowContainer}>ARROW GOES HERE</div>
-			</div>
-		</div>
-	);
-};
+class LandingPage extends Component {
+        state = {
+                hoverDescription: false
+        };
 
-export default landingPage;
+        handleHover = () => {
+                let hover = this.state.hoverDescription;
+                this.setState({ hoverDescription: !hover });
+        };
+
+        handleOut = () => {
+                alert('out');
+        };
+        render() {
+                return (
+                        <div className={cls.LoadingPageContainer}>
+                                <div className={cls.myInfoContainer}>
+                                        <h1 className={cls.myName}>Logan Wood</h1>
+                                        <p className={cls.myDescription} onMouseEnter={this.handleHover}>
+                                                Educator - Developer - Problem Solver
+                                        </p>
+                                        <div
+                                                className={cls.nextSectionArrowContainer}
+                                                hidden={this.state.hoverDescription}
+                                        >
+                                                ARROW GOES HERE
+                                        </div>
+                                </div>
+                        </div>
+                );
+        }
+}
+
+export default LandingPage;
