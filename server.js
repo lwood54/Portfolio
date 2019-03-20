d2a1ec617d5e5b4d79865d7e55f90c1794ed1a33
Logan Wood <lwood3499@gmail.com>
2019-03-19 21:50:43 -0500

    added proxy localhost removed app.get(/*)

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.get('/*', function(req, res) {
+//         res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 app.post('/api/form', (req, res) => {
         console.log(req.body);
