59894533c053cb32dfa74d3fcc866a8761f05c2c
Logan Wood <lwood3499@gmail.com>
2019-03-19 21:37:30 -0500

    connected express server to connect contact form

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const nodeMailer = require('nodemailer');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.post('/api/form', (req, res) => {
+        console.log(req.body);
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+        console.log(`Listening to server on port ${PORT}!`);
+});
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,51 +3,83 @@ import React, { useState } from 'react';
 import ScrollableAnchor from 'react-scrollable-anchor';
 
 import cls from './Contact.module.css';
+import axios from 'axios';
 
 const Contact = () => {
         const [name, setName] = useState('');
+        const [email, setEmail] = useState('');
         const [company, setCompany] = useState('');
-        const handleChange = (e, field) => {
-                switch (field) {
+        const [message, setMessage] = useState('');
+        const handleChange = e => {
+                switch (e.target.name) {
                         case 'name':
                                 setName(e.target.value);
                                 break;
+                        case 'email':
+                                setEmail(e.target.value);
+                                break;
                         case 'company':
                                 setCompany(e.target.value);
+                                break;
+                        case 'message':
+                                setMessage(e.target.value);
                                 break;
                         default:
                                 return;
                 }
         };
-        const handleSubmit = e => {
+        const handleSubmit = async e => {
                 e.preventDefault();
-                console.log('The name: ', name, 'was submitted.');
+                console.log(name, 'submitted a message from ', company, 'saying: ', message);
+                const form = await axios.post('api/form', {
+                        name,
+                        email,
+                        company,
+                        message
+                });
                 setName('');
+                setEmail('');
                 setCompany('');
+                setMessage('');
         };
         return (
                 <ScrollableAnchor id="contact">
                         <div className={cls.ContactContainer}>
-                                <form onSubmit={handleSubmit}>
-                                        <label>
-                                                Name:
-                                                <input
-                                                        type="text"
-                                                        name="name"
-                                                        value={name}
-                                                        onChange={e => handleChange(e, 'name')}
-                                                />
-                                        </label>
-                                        <label>
-                                                Company:
-                                                <input
-                                                        type="text"
-                                                        name="company"
-                                                        value={company}
-                                                        onChange={e => handleChange(e, 'company')}
-                                                />
-                                        </label>
-                                        <input type="submit" value="Submit" />
+                                <form onSubmit={handleSubmit} className={cls.FormContainer}>
+                                        <input
+                                                type="text"
+                                                className={cls.name}
+                                                name="name"
+                                                placeholder="Name"
+                                                value={name}
+                                                onChange={handleChange}
+                                        />
+                                        <input
+                                                type="email"
+                                                className={cls.email}
+                                                name="email"
+                                                placeholder="Email"
+                                                value={email}
+                                                onChange={handleChange}
+                                        />
+                                        <input
+                                                type="text"
+                                                className={cls.company}
+                                                name="company"
+                                                placeholder="Company"
+                                                value={company}
+                                                onChange={handleChange}
+                                        />
+                                        <textarea
+                                                type="textarea"
+                                                className={cls.message}
+                                                name="message"
+                                                placeholder="Message"
+                                                value={message}
+                                                onChange={handleChange}
+                                        />
+
+                                        <input type="submit" className={cls.submit} value="submit" />
                                 </form>
                         </div>
                 </ScrollableAnchor>
